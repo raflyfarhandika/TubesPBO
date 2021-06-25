71f1d8b5f64a7e1ad2398b6c4b8cabc0321b9f79
raflyfarhandika <raflyfarhandika@gmail.com>
2021-06-25 10:35:40 +0700

    Update DB_Connection

--- TUBESPBO7_/src/tubespbo7_/DB_Connection.java
+++ TUBESPBO7_/src/tubespbo7_/DB_Connection.java
@@ -15,16 +15,11 @@ public class DB_Connection {
     static final String DB_User = "root";
     static final String DB_Pass = "";
     static Connection conn;
-    static Statement statement;
-    static ResultSet result;
     //PreparedStatement ps;
     
     public static void Connection(){
         try {
             conn = DriverManager.getConnection(DB_url, DB_User, DB_Pass);
-            statement = conn.createStatement();
-            String Sql = "SELECT * FROM pegawai";
-            result = statement.executeQuery(Sql);
         } catch (Exception e){
             e.printStackTrace();
         }
